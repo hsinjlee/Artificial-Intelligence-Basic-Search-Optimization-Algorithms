c5d09e47f9470fdd10652ee43680ab7eba453f3e
hsinjlee <hlee294@asu.edu>
2020-07-31 15:27:51 -0700

    add bruteForce search

---
+++ bruteforceApp.java
@@ -0,0 +1,13 @@
+
+
+public class bruteforceApp {
+
+	public static void main(String[] args) {
+		
+		BruteForce bruteForce = new BruteForce();
+		
+		bruteForce.bruteForceSearch();
+		
+	}
+}
+//	The minimum value is f(x) = 1.7749370367472766E-30 and x = 1.0000000000000013
